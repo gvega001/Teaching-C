7acd4c1aeb087e36c194664698020fb45e040b4f
Gabriel Vega <gvega001@gmail.com>
2024-07-20 15:57:47 -0700

    stack & heap example

---
+++ projectfiles/stackHeapExamples.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void stackExample() {
+    int stackVar = 42;  // Stack variable
+    printf("Stack variable: %d\n", stackVar);
+}
+
+void heapExample() {
+    int* headVar == (int*)malloc(sizeof(int)); // Heap variable
+    if(headVar == NULL) {
+        fprintf(stderr, "Memory allocation failed \n");
+        return;
+    }
+    *headVar = 42;
+    printf("Heap variable: %d\n", *headVar);
+    free(headVar); // Free the allocated memory
+}
+
+int main() {
+    stackExample();
+    heapExample();
+    return 0;
+}
